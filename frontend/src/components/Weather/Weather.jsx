fede9da66ed06f173c240a9f56f5a0ed8d6bf6c2
Ropie1981 <pierre.saumont@gmail.com>
2023-05-11 23:29:25 +0200

    new weather + rendu liste à mapper

--- frontend/src/components/Weather/Weather.jsx
+++ frontend/src/components/Weather/Weather.jsx
@@ -6,7 +6,9 @@ import "./Weather.css";
 export default function Weather() {
   const { weatherData, setWeatherData } = React.useContext(WeatherContext);
   const [location, setLocation] = useState("");
+  const [items, setItems] = useState(null);
   const [showWeather, setShowWeather] = useState(false);
+  const [showLuggage, setShowLuggage] = useState(false);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f9f02a6789ca981ee3a69eeb7f8ce34e&units=metric&lang=fr`;
   const [date, setDate] = useState(new Date());
@@ -18,12 +20,19 @@ export default function Weather() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/vetements")
+      .then((res) => setItems(res.data));
+  }, []);
+
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
       axios.get(url).then((response) => {
         setWeatherData(response.data);
         setShowWeather(true);
+        setShowLuggage(true);
       });
       setLocation("");
     }
@@ -39,85 +48,101 @@ export default function Weather() {
   };
 
   return (
-    <div className="weather-card-container">
-      <div className="weather-card">
-        <div className="weather-top-container">
-          <div className="search">
-            <input
-              value={location}
-              onChange={handleLocationChange}
-              onKeyDown={searchLocation}
-              onClick={handleInputClick}
-              placeholder="Enter Location"
-              type="text"
-              className="search-input"
-            />
-            <button
-              type="button"
-              className="submit-location-btn"
-              onClick={() => {
-                axios.get(url).then((response) => {
-                  setWeatherData(response.data);
-                  setShowWeather(true);
-                });
-                setLocation("");
-              }}
-            >
-              Search
-            </button>
+    <>
+      <div className="weather-card-container">
+        <div className="weather-card">
+          <div className="weather-top-container">
+            <div className="search">
+              <input
+                value={location}
+                onChange={handleLocationChange}
+                onKeyDown={searchLocation}
+                onClick={handleInputClick}
+                placeholder="Enter Location"
+                type="text"
+                className="search-input"
+              />
+              <button
+                type="button"
+                className="submit-location-btn"
+                onClick={() => {
+                  axios.get(url).then((response) => {
+                    setWeatherData(response.data);
+                    setShowWeather(true);
+                    setShowLuggage(true);
+                  });
+                  setLocation("");
+                }}
+              >
+                Search
+              </button>
+            </div>
+            {showWeather && weatherData && (
+              <>
+                <div className="location-date">
+                  <div className="location">
+                    <p>{weatherData.name}</p>
+                  </div>
+                  <div className="date">
+                    <p>{date.toDateString("fr-FR")}</p>
+                  </div>
+                </div>
+                <div className="temp">
+                  {weatherData.main ? (
+                    <h2>{weatherData.main.temp.toFixed()}°c</h2>
+                  ) : null}
+                </div>
+                <div className="weather-description">
+                  {weatherData.weather ? (
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                      alt="weather-icon"
+                      className="weather-icon"
+                    />
+                  ) : null}
+                  {weatherData.weather ? (
+                    <p>{weatherData.weather[0].description}</p>
+                  ) : null}
+                </div>
+              </>
+            )}
           </div>
+
           {showWeather && weatherData && (
-            <>
-              <div className="location-date">
-                <div className="location">
-                  <p>{weatherData.name}</p>
-                </div>
-                <div className="date">
-                  <p>{date.toDateString("fr-FR")}</p>
-                </div>
-              </div>
-              <div className="temp">
+            <div className="weather-bottom-container">
+              <div className="feels">
                 {weatherData.main ? (
-                  <h2>{weatherData.main.temp.toFixed()}°c</h2>
+                  <p>{weatherData.main.feels_like.toFixed()}°C</p>
                 ) : null}
+                <p>Ressenti</p>
               </div>
-              <div className="weather-description">
-                {weatherData.weather ? (
-                  <img
-                    src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-                    alt="weather-icon"
-                    className="weather-icon"
-                  />
-                ) : null}
-                {weatherData.weather ? (
-                  <p>{weatherData.weather[0].description}</p>
-                ) : null}
+              <div className="humidity">
+                {weatherData.main ? <p>{weatherData.main.humidity}%</p> : null}
+                <p>Humidité</p>
               </div>
-            </>
+              <div className="wind">
+                {weatherData.wind ? (
+                  <p>{weatherData.wind.speed.toFixed()}km/h</p>
+                ) : null}
+                <p>Vent</p>
+              </div>
+            </div>
           )}
         </div>
-
-        {showWeather && weatherData && (
-          <div className="weather-bottom-container">
-            <div className="feels">
-              {weatherData.main ? (
-                <p>{weatherData.main.feels_like.toFixed()}°C</p>
-              ) : null}
-              <p>Ressenti</p>
-            </div>
-            <div className="humidity">
-              {weatherData.main ? <p>{weatherData.main.humidity}%</p> : null}
-              <p>Humidité</p>
-            </div>
-            <div className="wind">
-              {weatherData.wind ? (
-                <p>{weatherData.wind.speed.toFixed()}km/h</p>
-              ) : null}
-              <p>Vent</p>
-            </div>
-          </div>
-        )}
       </div>
-    </div>
+
+      {showLuggage && (
+        <div className="luggage">
+          {/* <ul>
+            {items.map((item) => (
+              <li key={item.vetements[0].vetements_tres_froid.id}>
+                {item.vetements[0].vetements_tres_froid.name}
+              </li>
+            ))}
+            </ul> */}
+          <p>{items.vetement_tres_froid[0].name}</p>
+        </div>
+      )}
+    </>
   );
 }
