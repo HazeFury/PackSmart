85fc3d9cb043c73947dacdfc56e1c6ac7b5d015b
Aurélie <aurelie.demure.ad@gmail.com>
2023-05-12 11:01:42 +0200

    added css on home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,18 +9,23 @@ export default function Home() {
       <div className="home-container">
         <img id="home-image" src={backgroundImg} alt="voyage" />
         <h2 className="heading">
-          Vous voulez partir en vacances mais vous ne savez pas quoi emmener ?
+          Want to go on vacation but don't know what to pack?
         </h2>
         <p className="description">
-          Notre application simplifie la préparation des bagages pour vos
-          vacances. Renseignez votre destination et le type de vacance et nous
-          vous fournissons une liste personnalisée de vêtements indispensables
-          selon les conditions météorologiques. Voyagez léger et concentrez-vous
-          sur les choses importantes - explorer, se détendre et créer des
-          souvenirs inoubliables!
+          Our application simplifies the preparation of the{" "}
+          <span>luggage </span>
+          for your holidays.
+          <br /> All you have to do is fill in your <span>destination</span> and
+          the type of vacation you are planning, and we will take care of the
+          rest! We will provide you with a <span>personalized list</span> of
+          <span>must-have clothing</span> depending on{" "}
+          <span>the weather conditions.</span> Travel light and focus on the
+          important things - exploring, relaxing and creating unforgettable
+          memories!
+          <span className="sun">&#x1F31E;</span>
         </p>
         <Link to="/packing" className="button">
-          PackSmart
+          Let's PackSmart!
         </Link>
       </div>
     </div>
